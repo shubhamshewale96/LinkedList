ba8ec0b3c5b1f9d619f4c6c0266fbd26ff952a17
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-16 14:51:54 +0530

    add:create simple linked list

---
+++ src/test/java/MyNodeTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bl.Linked.MyNode;
+
+public class MyNodeTest {
+	@Test
+    public void given3NumbersLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+}
